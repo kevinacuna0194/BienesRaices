f063b33b95cc6f27ec85f87aa90fb1dcac5415a5
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-01-25 13:47:44 -0300

    Creadno una tabla para listar las propiedades de la BD

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,7 @@
 
 $resultado = $_GET['resultado'] ?? null; // isset($_GET['resultado'])
 
+include '../includes/funciones.php';
 incluirTemplate('header');
 ?>
 
@@ -12,6 +13,31 @@ incluirTemplate('header');
     <?php endif; ?>
 
     <a href="/admin/propiedades/crear.php" class="botom boton-verde">Nueva Propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Casa en la playa</td>
+                <td> <img src="../imagenes/0ac34ab5543d74fa192df2f73b264819.jpg" alt="Imagen Tabla" class="imagen-tabla"> </td>
+                <td>120000</td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 <?php
