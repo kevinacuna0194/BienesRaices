4c8d0f9d449689287a312a73b7274b282917a68b
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-01-26 18:29:01 -0300

    Crear una tabla para usuarios

---
+++ usuarios.php
@@ -0,0 +1,15 @@
+<?php
+
+/** Importar la conexión */
+require 'includes/config/database.php';
+$db = conectarDB();
+
+/** Crear un email y password */
+$email = "correo@correo.com";
+$password = "123456";
+
+/** Query para crear un usuario */
+$query = "INSERT INTO usuarios (email, password) VALUES ('{$email}', '{$password}')";
+
+/** Agregarlo a la BD */
+mysqli_query($db, $query);
