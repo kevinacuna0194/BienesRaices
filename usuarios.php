2efb09df6eb84a738dfa5930475d6b9c0b164920
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-01-26 18:29:52 -0300

    Hashear Passwords

--- usuarios.php
+++ usuarios.php
@@ -8,8 +8,12 @@ $db = conectarDB();
 $email = "correo@correo.com";
 $password = "123456";
 
+/** Toma dos argumentos 1- Password que vamos a hashear. 2- Algoritmo que se va a utilizar para hashear este password. **/
+// $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+$passwordHash = password_hash($password, PASSWORD_BCRYPT); // string(60) "$2y$10$DD/.iVjTyYkSl7m5zHTWPOmPPrOiCH/upSCI2Ajrski8T49ldnI0e"
+
 /** Query para crear un usuario */
-$query = "INSERT INTO usuarios (email, password) VALUES ('{$email}', '{$password}')";
+$query = "INSERT INTO usuarios (email, password) VALUES ('{$email}', '{$passwordHash}')";
 
 /** Agregarlo a la BD */
 mysqli_query($db, $query);
