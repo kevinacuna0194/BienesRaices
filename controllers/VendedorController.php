c846ae500e160fa9f5a1eb00e85103bf86b0ea39
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-02-14 15:57:31 -0300

    Creando el Controlador para Vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController {
+
+    public static function crear() {
+        echo 'Crear Vendedor';
+    }
+
+    public static function actualizar() {
+        echo 'Actualizar Vendedor';
+    }
+
+    public static function eliminar() {
+        echo 'Eliminar Vendedor';
+    }
+}
