202e94654b4e0b529e4e131f127b0c09535024e1
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-02-16 16:23:07 -0300

    Ocultar o Mostrar los campos del formulario según la selección del usuario

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     eventListeners();
 
@@ -11,14 +11,14 @@ function darkMode() {
 
     // console.log(prefiereDarkMode.matches);
 
-    if(prefiereDarkMode.matches) {
+    if (prefiereDarkMode.matches) {
         document.body.classList.add('dark-mode');
     } else {
         document.body.classList.remove('dark-mode');
     }
 
-    prefiereDarkMode.addEventListener('change', function() {
-        if(prefiereDarkMode.matches) {
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
             document.body.classList.add('dark-mode');
         } else {
             document.body.classList.remove('dark-mode');
@@ -26,19 +26,45 @@ function darkMode() {
     });
 
     const botonDarkMode = document.querySelector('.dark-mode-boton');
-    botonDarkMode.addEventListener('click', function() {
+    botonDarkMode.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode');
     });
 }
 
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
-
     mobileMenu.addEventListener('click', navegacionResponsive);
+
+    const metodoContacto = document.querySelectorAll('input[name="contacto[contacto]"]');
+    metodoContacto.forEach(input => input.addEventListener('click', seleccionarMetodo));
 }
 
 function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion');
 
     navegacion.classList.toggle('mostrar')
+}
+
+function seleccionarMetodo(e) {
+    const contactoDiv = document.querySelector('#contacto');
+
+    if (e.target.value === 'telefono') {
+        contactoDiv.innerHTML = `
+        <label for="telefono">Número Teléfono</label>
+        <input type="tel" placeholder="Tu Teléfono" id="telefono" name="contacto[telefono]">
+
+        <p>Elija la fecha y la hora para la llamada</p>
+
+            <label for="fecha">Fecha:</label>
+            <input type="date" id="fecha" name="contacto[fecha]">
+
+            <label for="hora">Hora:</label>
+            <input type="time" id="hora" min="09:00" max="18:00" name="contacto[hora]">
+        `;
+    } else {
+        contactoDiv.innerHTML = `
+        <label for="email">E-mail</label>
+        <input type="email" placeholder="Tu Email" id="email" name="contacto[email]">
+        `;
+    }
 }
