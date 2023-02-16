b8a8e0c3db652c17afa0e7e96b6ab080c60fac0d
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-02-15 22:18:13 -0300

    Enviar un Email con los datos del formulario

--- controllers/PaginasControllers.php
+++ controllers/PaginasControllers.php
@@ -4,11 +4,12 @@ namespace Controllers;
 
 use MVC\Router;
 use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class PaginasControllers
 {
     public static function index(Router $router)
-    {   
+    {
         $propiedades = Propiedad::get(3);
         $inicio = true;
 
@@ -24,7 +25,7 @@ class PaginasControllers
     }
 
     public static function propiedades(Router $router)
-    {   
+    {
         $propiedades = Propiedad::all();
 
         $router->render('paginas/propiedades', [
@@ -56,11 +57,39 @@ class PaginasControllers
     public static function contacto(Router $router)
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            debuguear($_POST);
+            /** Crar instancia de PHPMailer */
+            $phpmailer = new PHPMailer();
+
+            /** Configurar SMTP (protocolo para envio de emails) */
+            $phpmailer->isSMTP();
+            $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
+            $phpmailer->SMTPAuth = true;
+            $phpmailer->Username = '5b9ec453cd6e12';
+            $phpmailer->Password = '169fc04abc29c3';
+            $phpmailer->SMTPSecure = 'tls'; // Encriptación
+            $phpmailer->Port = 2525;
+
+            /** Configurar el contenido del mail */
+            $phpmailer->setFrom('admin@bienesraices.com');
+            $phpmailer->addAddress('admin@bienesraices.com', 'BienesRaices.com');
+            $phpmailer->Subject = 'Tienes un Nuevo Mensaje';
+
+            /** Habilitar HTML */
+            $phpmailer->isHTML(true);
+            $phpmailer->CharSet = 'UTF-8';
+
+            /** Definir el contenido */
+            $contenido = '<html> <p> Tienes un nuevo mensaje </p> </html>';
+            $phpmailer->Body = $contenido;
+            $phpmailer->AltBody = 'Texto alternativo sin HTML';
+
+            if ($phpmailer->send()) {
+                echo "Mensaje enviado Correctamente";
+            } else {
+                echo "El mensaje no se puedo enviar, Mailer Error: {$phpmailer->ErrorInfo}";
+            }
         }
 
-        $router->render('paginas/contacto', [
-
-        ]);
+        $router->render('paginas/contacto', []);
     }
 }
